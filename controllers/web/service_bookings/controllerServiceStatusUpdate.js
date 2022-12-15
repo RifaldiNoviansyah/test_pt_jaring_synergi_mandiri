63198db7548577d6681b8fceb34e31502d8ad140
RifaldiNoviansyah <rifaldinoviansyah11@gmail.com>
2022-12-15 15:40:19 +0700

    fix validation

--- controllers/web/service_bookings/controllerServiceStatusUpdate.js
+++ controllers/web/service_bookings/controllerServiceStatusUpdate.js
@@ -1,5 +1,5 @@
 // helpers
-const { internalServerError, success } = require('@helpers/errorResponse')
+const { internalServerError, success, notFound } = require('@helpers/errorResponse')
 // query
 const { queryGetOneServiceBooking } = require('@query/service_bookings/findOne')
 const { queryGetOneServiceStatus } = require('@query/service_statuses/findOne')
@@ -12,7 +12,6 @@ module.exports = {
     const serviceStatusId = req.params.serviceStatusId
     const linkApi = `/api/web/service_booking/${serviceBookingId}/status_service/${serviceStatusId} -update-`
     try {
-
       const serviceBookingData = await queryGetOneServiceBooking(linkApi, dealerId, serviceBookingId, res)
       if(serviceBookingData !== undefined){
         const serviceStatusData = await queryGetOneServiceStatus(linkApi, dealerId, serviceStatusId, res)
@@ -23,24 +22,32 @@ module.exports = {
                 serviceScheduleData.updated_at = new Date()
                 await serviceScheduleData.save()
             }
+          
+          const dataScheduleBookindDate = new Date(serviceBookingData.serviceSchedules.schedule_date).getDate()
+          const dateNow = new Date().getDate()
 
-          serviceBookingData.service_status_id = serviceStatusId,
-          serviceBookingData.updated_at = new Date()
-          await serviceBookingData.save()
-
-          const result = {
-            id: serviceBookingData.id,
-            name: serviceBookingData.name,
-            phoneNo: serviceBookingData.phone_no,
-            vehicleType: serviceBookingData.vehicle_type,
-            licensePlate: serviceBookingData.license_plate,
-            vehicleProblem: serviceBookingData.vehicle_problem,
-            serviceScheduleId: serviceBookingData.service_schedule_id,
-            serviceTime: serviceBookingData.service_time,
-            serviceStatusId: serviceBookingData.service_status_id,
-            delStatus: serviceBookingData.del_status
+          if(dataScheduleBookindDate !== dateNow && serviceStatusData.name === "datang"){
+             return notFound(res, 'Edit Ke Status ke Datang Hanya Bisa Di Ubah Di Hari H', serviceStatusData.name, linkApi, dealerId)
+          }else{
+            serviceBookingData.service_status_id = serviceStatusId,
+            serviceBookingData.updated_at = new Date()
+            await serviceBookingData.save()
+            const result = {
+              id: serviceBookingData.id,
+              name: serviceBookingData.name,
+              phoneNo: serviceBookingData.phone_no,
+              vehicleType: serviceBookingData.vehicle_type,
+              licensePlate: serviceBookingData.license_plate,
+              vehicleProblem: serviceBookingData.vehicle_problem,
+              serviceScheduleId: serviceBookingData.service_schedule_id,
+              serviceTime: serviceBookingData.service_time,
+              serviceStatusId: serviceBookingData.service_status_id,
+              delStatus: serviceBookingData.del_status
+            }
+            return success(res, 'Ubah Service Booking Berhasil', result, linkApi, dealerId)
           }
-          return success(res, 'Ubah Service Booking Berhasil', result, linkApi, dealerId)
+
+
         }
       }
     } catch (error) {
